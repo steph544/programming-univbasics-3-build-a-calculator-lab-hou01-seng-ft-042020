39069a9532e17d4ec5ae4de91a8df8780e1231cc
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-19 05:43:19 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,27 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+sub = num1 - num2
+sub
 end
 
 def division(num1, num2)
-
+div = num1/num2
 end
 
 def multiplication(num1, num2)
-
+mult = num1 * num2
+mult
 end
 
 def modulo(num1, num2)
-
+mod = num1 % num2
 end
 
 def square_root(num)
-
+square = num*2
+square
 end
